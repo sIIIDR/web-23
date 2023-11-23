bb7e9455ca43a5ddd79202e6308848b0e9645fbc
sIIIDR <kirill.sidorenkov.2003@yandex.ru>
2023-11-23 07:01:47 +0400

    new page

---
+++ example-app-web-23/resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<div class="w-full bg-white  px-[5%] py-[10px] flex flex-row">
+    <img src="{{ asset('assets/logo.png') }}" class="w-[70px] h-[70px]" />
+    <div class="w-full flex align-center justify-center">
+        <button class="uppercase hover:text-blue-500">О нас</button>
+        <button class="px-[5%] uppercase hover:text-blue-500">Подарки</button>
+        <button class="uppercase hover:text-blue-500">Форма связи</button>
+    </div>
+    <div class="flex flex-col justify-center">
+        <a href="" class="hover:underline">+7 967 715 24 99</a>
+        <a href="" class="hover:underline">kirill.sidorenov.2003@yandex.ru</a>
+    </div>
+</div>
---
+++ example-app-web-23/resources/views/components/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    @livewireStyles
+</head>
+
+<body>
+    <img src="{{ asset('assets/background.jpg') }}"
+        class="bg-no-repeat bg-fixed object-cover blur-md absolute top-0 -z-10 h-screen" />
+    <x-header />
+    {{ $slot }}
+    @livewireScripts
+</body>
+
+</html>
